6537b2a8d0e9110a15488f5441b36624a9d0d7cb
liutongchao <413281269@qq.com>
2017-04-25 15:02:31 +0800

    ADD

---
+++ auto.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+#计时
+SECONDS=0
+
+#update Pods
+#rm -rf Pods
+#rm Podfile.lock
+#pod update
+
+#假设脚本放置在与项目相同的路径下
+project_path="xxx"
+#取当前时间字符串添加到文件结尾
+now=$(date +"%Y%m%d%H_%M%S")
+
+#指定项目的scheme名称
+scheme="EducateMonitor"
+#指定要打包的配置名
+configuration="Debug"
+#指定项目地址
+workspace_path="$project_path/xxx.xcworkspace"
+#指定输出路径
+output_path="/Users/xxx/Dev/ipa"
+#指定输出归档文件地址
+archive_path="$output_path/xxx_${now}.xcarchive"
+#指定输出ipa地址
+ipa_name="xxx-${now}.ipa"
+#ipa路径
+ipa_path="$output_path/$ipa_name"
+#指定打包所使用的输出方式，目前支持app-store, package, ad-hoc, enterprise, development, 和developer-id，即xcodebuild的method参数
+export_method='development'
+#获取执行命令时的commit message
+commit_msg="$1"
+
+
+#输出设定的变量值
+echo "===workspace path: ${workspace_path}==="
+echo "===archive path: ${archive_path}==="
+echo "===ipa path: ${ipa_path}==="
+echo "===profile: ${provisioning_profile}==="
+echo "===commit msg: $1==="
+
+#先清空前一次build
+gym --workspace ${workspace_path} --scheme ${scheme} --clean --configuration ${configuration} --archive_path ${archive_path} --export_method ${export_method} --output_directory ${output_path} --output_name ${ipa_name}
+
+#判断ipa是否生成 如果没有生成 shell报错=>告知Job构建失败
+if [ -f $ipa_path ]
+then
+    echo "Generate $ipa_path successfully!"
+else
+    echo "Generate $ipa_path fail!"
+    exit 1
+fi
+
+#上传到fir
+#fir publish ${ipa_path} -T fir_token -c "${commit_msg}"
+
+#输出总用时
+echo "===Finished. Total time: ${SECONDS}s==="
